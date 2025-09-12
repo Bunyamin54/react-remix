cab2e1f5168c777c5d30a0baccbb58079085eb39
Bunyamin Gec <bynnngnc.1660@gmail.com>
2025-09-12 13:27:51 +0200

    eksempels for componets and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,65 @@
 import './App.css'
 
-function App() {
+// function App() {
 
-const name =  <h1>Bunyamin</h1>
+// const name =  <h1>Bunyamin</h1>
+// const age= <h2> 24 </h2>
+// const email = <h3> bunya@gmail.com </h3>
 
-const age= <h2> 24 </h2>
+// const user =  <div>
 
-  return (
+//   {name}
+//   {age}
+//   {email}
+// </div>  
+    
 
-     <div className= "App"> 
-     
-      <h1> {name}</h1>
-      <h2> {age} </h2>
-   
+//   return (
 
-     </div>
-  )
-}
+//      <div className= "App"> 
+  
+//     <User name = "Bunyamin"  age={24}  email="bunya@gmail.com" />
+//     <User name = "Perdo"  age={21}  email="perdo@gmail.com" />
+//     <User name = "John"  age={30}  email="john@gmail.com" />
+
+//      </div>
+//   )
+// }
+
+
+//   const User = (props) => {
+//     return (
+//       <div>
+//     <h1>{props.name}</h1>
+//     <h2>{props.age}</h2>
+//     <h3>{props.email}</h3>
+//    </div>
+//     )
+//   }
+
+  // function App () {
+  //   return (
+  //    < div className = "App">
+  //     <Job salary = {5000}  position="developer"  company="Google" />
+  //     <Job salary = {4000}  position="tester"  company="Microsoft" />
+  //     <Job salary = {6000}  position="team lead"  company="Amazon" />
+  //    </div>
+  //   )
+  // }
+
+  //   const Job =(props) => {
+  //     return (
+  //        <div>
+  //       <h1>{props.salary}</h1>
+  //       <h2>{props.position}</h2>
+  //       <h3>{props.company}</h3>
+  //        </div>
+
+  //     )
+
+  //   }
+
+  
+
 
 export default App
