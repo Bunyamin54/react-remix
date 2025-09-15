cc50722aab4d65ed787f4547fd1203e1cffe6e54
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-15 12:45:46 +0200

    jsx component

--- 2-comp-jsx/src/App.js
+++ 2-comp-jsx/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl font-bold underline'>My Remix App</h1>
+      <Header/>
     </div>
   );
 }
