904c4ef4269f03915a5e2c3c24a5feb26297fe10
Bunyamin Gec <bnymngnc.1660@gmail.com>
2025-09-15 10:19:30 +0200

    jsx in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+
+ // Component 
+
 function App() {
+
+   // JS kodu yazabiliyoruz
+
+   const deneme = "React"
+
+
   return (
     <div className="App">
       <h1>My Remix App</h1>
+      <p>{deneme} e hosgeldiniz</p>
     </div>
   );
 }
