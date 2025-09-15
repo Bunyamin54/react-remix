39ec6766f6b721bc9cc35c431b392b427775117b
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-15 12:51:04 +0200

    components

--- 2-comp-jsx/src/App.js
+++ 2-comp-jsx/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
+import { Main } from './Main';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl font-bold underline'>My Remix App</h1>
+
       <Header/>
+      <Main/>
       <Footer/>
     </div>
   );
