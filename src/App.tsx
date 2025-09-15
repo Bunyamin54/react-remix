dd50160eb53c9d2a71e2c9c6de0e9530811e4fa8
Bunyamin Gec <bnymngnc.1660@gmail.com>
2025-09-15 08:49:37 +0200

    functions react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 
 // function App() {
 
@@ -11,13 +11,12 @@ import './App.css'
 //   {name}
 //   {age}
 //   {email}
-// </div>  
-    
+// </div>
 
 //   return (
 
-//      <div className= "App"> 
-  
+//      <div className= "App">
+
 //     <User name = "Bunyamin"  age={24}  email="bunya@gmail.com" />
 //     <User name = "Perdo"  age={21}  email="perdo@gmail.com" />
 //     <User name = "John"  age={30}  email="john@gmail.com" />
@@ -25,7 +24,6 @@ import './App.css'
 //      </div>
 //   )
 // }
-
 
 //   const User = (props) => {
 //     return (
@@ -37,29 +35,44 @@ import './App.css'
 //     )
 //   }
 
-  function App () {
-    // return (
-    //  < div className = "App">
-    //   <Job salary = {5000}  position="developer"  company="Google" />
-    //   <Job salary = {4000}  position="tester"  company="Microsoft" />
-    //   <Job salary = {6000}  position="team lead"  company="Amazon" />
-    //  </div>
-    // )
-  }
+//   function App () {
+//     // return (
+//     //  < div className = "App">
+//     //   <Job salary = {5000}  position="developer"  company="Google" />
+//     //   <Job salary = {4000}  position="tester"  company="Microsoft" />
+//     //   <Job salary = {6000}  position="team lead"  company="Amazon" />
+//     //  </div>
+//     // )
+//   }
 
-    // const Job =(props) => {
-    //   return (
-    //      <div>
-    //     <h1>{props.salary}</h1>
-    //     <h2>{props.position}</h2>
-    //     <h3>{props.company}</h3>
-    //      </div>
+//     // const Job =(props) => {
+//     //   return (
+//     //      <div>
+//     //     <h1>{props.salary}</h1>
+//     //     <h2>{props.position}</h2>
+//     //     <h3>{props.company}</h3>
+//     //      </div>
 
-    //   )
+//     //   )
 
-    // }
+//     // }
 
-  
+// export default App
 
+function App() {
+  const age = 19;
+  const isGreen = true;
 
-export default App
+  return <div className="name">{age >= 18 ? "Over age" : "Under age"}
+     <h1 style={{color: isGreen ? "green" : "red"}}> This Has Color </h1>
+  </div>;
+
+  // if (age >= 18) {
+  //   return <h1 className="name">Over age</h1>;
+  // } else {
+  //   return <div className="name">Under age</div>;
+  // }
+
+}
+
+export default App;
