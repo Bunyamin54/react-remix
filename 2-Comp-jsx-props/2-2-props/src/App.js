57a045f4f945e190f9eb2e4e178600a31c2dbf64
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-16 17:48:10 +0200

    props

--- 2-Comp-jsx-props/2-2-props/src/App.js
+++ 2-Comp-jsx-props/2-2-props/src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 
-import Person from "./components/Person";
+import Cat from "./components/Cat";
 
 function App() {
   return (
     <div>
-      <Person
+      <Cat
         name="Cenitin"
         img="https://cdn.pixabay.com/photo/2023/06/22/15/17/cat-8081701_640.jpg"
         color="Beyaz"
       />
-      <Person
+      <Cat
         name="Garfld"
         img="https://cdn.pixabay.com/photo/2017/07/22/15/21/cat-2528935_640.jpg"
         color="gray"
       />
-      <Person
+      <Cat
         name="Felix"
         img="https://cdn.pixabay.com/photo/2016/03/28/10/05/kitten-1285341_640.jpg"
        
