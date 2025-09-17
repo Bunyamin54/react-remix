96915c85d076a25a19551d18443f8c36512efb09
Bünyamin Genc <bynnngnc.1660@gmail.com>
2025-09-17 13:42:18 +0200

    Sass

--- 3-1-sass/src/components/Header.jsx
+++ 3-1-sass/src/components/Header.jsx
@@ -2,9 +2,9 @@ import HeaderStyle from "../scss/header.module.scss"
 
 const Header = () => {
   return (
-    <div className={HeaderStyle["header"]}>
+    
       <h1>COMMENTS</h1>
-    </div>
+  
   )
 }
 
