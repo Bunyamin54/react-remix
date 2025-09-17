7050dce99d948980065462da8ed3bf542e69d08d
Bünyamin Genc <bynnngnc.1660@gmail.com>
2025-09-17 22:30:46 +0200

    useState Hook

--- 4-Events-useStateHook/src/3-useState/UseStateCounter.jsx
+++ 4-Events-useStateHook/src/3-useState/UseStateCounter.jsx
@@ -4,7 +4,7 @@
 //! React 16.8 versiyonu ile gelmistir ve geldikten sonra Class-componentler'in
 //! kullanimi cok azaltmistir.
 
-import { useState } from "react"
+import { useState } from "react";
 
 //? React'ta useState(), useEffect(), useContext() gibi bir cok built-in
 //? Hook bulunmaktadir. Ayrica custom hook tanimlamak da mumkundur.
@@ -20,51 +20,50 @@ import { useState } from "react"
 //?    https://react.dev/reference/react
 //* =============================================================
 
- 
-
 const UseStateCounter = () => {
-//  let count = 0
+  //  let count = 0
 
-const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
 
   //! count adinda bir state tanimlamis olduk ve baslangic degerine 0 atadik.
 
   const handelInc = () => {
-
     //  count = count + 1
 
-    setCount(count +1)
+    setCount(count + 1);
 
     //  console.log(count)
-
-  }
-
+  };
 
 
+   const handleDec = () => {
+
+    if(count<=0) {
+     alert("count can not be less then 0 ")
+    }else
+      setCount(count -1 )
+
+   }
   return (
     <div>
-
-  <h2>USE STATE HOOK</h2>
-  <h1>Count:{count}</h1>
-  <button onClick={handelInc}>INC</button>
-  <button onDoubleClick={() => setCount(0)}>CLR</button>
-  <button onClick={() => setCount(count - 1 )}>DEC</button>
-
-    
+      <h2>USE STATE HOOK</h2>
+      <h1>Count:{count}</h1>
+      <button onClick={handelInc}>INC</button>
+      <button onDoubleClick={() => setCount(0)}>CLR</button>
+      {/* <button onClick={() => setCount(count - 1)}>DEC</button> */}
+      <button onClick={handleDec}>DEC</button>
     </div>
-  )
-}
+  );
+};
 
-export default UseStateCounter
+export default UseStateCounter;
 
+// <h2>USE STATE HOOK</h2>
+//     <h1>Count:{count}</h1>
+//     <button onClick={handleInc}>INC</button>
+//     <button onDoubleClick={() => setCount(0)}>CLR</button>
 
-  // <h2>USE STATE HOOK</h2>
-  //     <h1>Count:{count}</h1>
-  //     <button onClick={handleInc}>INC</button>
-  //     <button onDoubleClick={() => setCount(0)}>CLR</button>
+//     {/* Eger setCount metodunu callback ile yazmazsak bu metot ilk acilistan itibaren doğrudan cagirilmis olur. Bu durumda da count state'inin gunceller. State guncellendigi icinde component re-render olur. Re-render ise yeninden setCount araciligi state'in guncellenmesine yol acar ve sonsuz donguye girer */}
 
-  //     {/* Eger setCount metodunu callback ile yazmazsak bu metot ilk acilistan itibaren doğrudan cagirilmis olur. Bu durumda da count state'inin gunceller. State guncellendigi icinde component re-render olur. Re-render ise yeninden setCount araciligi state'in guncellenmesine yol acar ve sonsuz donguye girer */}
-
-  //     {/* <button onClick={handleDec}>DEC</button> */}
-  //     <button onClick={() => count > 0 && setCount(count - 1)}>DEC</button>
+//     {/* <button onClick={handleDec}>DEC</button> */}
+//     <button onClick={() => count > 0 && setCount(count - 1)}>DEC</button>
