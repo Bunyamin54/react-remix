683fab75bc1d06299490bb18c3ebb9bcbc7cf723
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-17 16:07:25 +0200

    sasss module css

--- 3-1-sass/src/components/Card.jsx
+++ 3-1-sass/src/components/Card.jsx
@@ -1,11 +1,29 @@
-import CardStyle from "../scss/card.module.scss"
+import CardStyle from "../scss/card.module.scss";
 
 const Card = ({ data }) => {
-  console.log(data)
+  console.log(data);
   //? js
-  return (
-   <div></div>
-  )
-}
 
-export default Card
+  return (
+    <div className={CardStyle.container}>
+      {data.map((item) => {
+        const { id, name, job, img, comment } = item;
+
+        return (
+          <div key={id} className={CardStyle.card}>
+            <h1>{name}</h1>
+            <h3>{job}</h3>
+            <p>{comment}</p>
+            <img src={img} alt={name} />
+            <div>
+              <button className={CardStyle.small}>Small</button>
+              <button className={CardStyle.large}>Large</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
