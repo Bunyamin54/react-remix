6af3b1f726a1ee891fd58422d80854d104aacff3
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-21 14:43:35 +0200

    onChange events in React

--- 5-Forms-otherEvents/src/components/1-forms/Form.jsx
+++ 5-Forms-otherEvents/src/components/1-forms/Form.jsx
@@ -80,36 +80,46 @@
 
 // export default Form
 
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handelUserName = (e) => {
+    setUsername(e.target.value);
+  };
+
   return (
     <div className="container mt-4">
       <h2 className="text-center text-success">Forms in React</h2>
       <form>
-           <div className="mb-3">
+        <div className="mb-3">
           <label htmlFor="username" className="form-label">
-            Hello
+            Hello {username}
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
             id="username"
             aria-describedby="emailHelp"
+            // onCahnge eventi inputun degeri degistiginde tetiklenir. bizde yazdigimzi eventhandler ile statimizi guncelleriz
+            onChange={handelUserName}
+            value={username} // value propu inputlara baslangic degeri vermemizi saglar..
           />
-         
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email address
+            Email address : <span className="text-danger">{email}</span>
           </label>
           <input
             type="email"
             className="form-control"
             id="email"
             aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
           />
-         
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
@@ -119,15 +129,14 @@ const Form = () => {
             type="password"
             className="form-control"
             id="password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="text-center">
-            <button type="submit" className="btn btn-success">
-          Submit
-        </button>
-
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
         </div>
-      
       </form>
     </div>
   );
