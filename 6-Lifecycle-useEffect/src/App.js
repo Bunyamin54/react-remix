93b01fc4797896faf27046c482ccfceb4a6bcd24
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-24 10:07:28 +0200

    useEffect hook

---
+++ 6-Lifecycle-useEffect/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import LifeCycleMethods from './components/LifeCycleMethods'
+import UseEffectHook from './components/UseEffectHook'
+
+function App() {
+  const [visible, setVisible] =useState(false)
+  return (
+    <div className='container text-center mt-4'>
+      <button className='btn btn-danger' onClick={() => setVisible(!visible)}>Show/Hide</button>
+  
+    {/* {visible && <LifeCycleMethods/>} */}
+    {visible && <UseEffectHook/>}
+    
+
+    </div>
+  )
+}
+
+export default App
---
+++ 6-Lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -0,0 +1,143 @@
+// import { useState, useEffect } from "react"
+
+// //?===============================================
+// //?                USEEFFECT HOOK
+// //?===============================================
+// //! UseEffect Hook'u fonksiyonel componenler'te yan etkileri
+// //! (side effect) gerceklestirmek icin kullanilir.
+// //! componentDidMount,componentDidUpdate,ve componentWillUnmount
+// //! metotlarinin bir birlesimi gibi dusunulebilir.
+
+// //! useEffect(() => {
+// //*   /* ComponentDidMount code */
+// //*      Kodlar
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   */ ComponentDidMount + componentDidUpdate code */
+// //! }, [state1, state2]);
+
+// //! useEffect(() => {
+// //?   /* ComponentDidMount code */
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   //* componentDidMount code + componentDidUpdate code */
+
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, [state1, state2]); //? Dependency Array
+
+// //   useEffect(() => {
+// //     //? componentDidMount (Dependency Array bos ise)
+// //     //? componentDidMount + componentDidUpdate (Dependency array bos degilse)
+// //     //? Dependecny array'Deki state her degistiginde veya ilk render sonrasi bu kod blogu tekrardan calistirilir.
+
+// //     return () => {
+// //       //? Cleanup function (componentWillUnmount)
+// //     }
+// //   }, [count]) //? Dependency Array
+
+// const UseEffectHook = () => {
+//   const [count, setCount] = useState(0)
+
+//   //!   ComponentDidMount
+//   //   useEffect(() => {
+//   //     //! fetch, async-await ,localStorage, setTimeout, setInterval();
+//   //     console.log("componentDidMount")
+//   //     setTimeout(() => {
+//   //       alert("Data fetched")
+//   //     }, 3000)
+//   //   }, []) //? Dependecny array bos
+
+//   //!   ComponentDidUpdate
+//   //   useEffect(() => {
+//   //     console.log("componentDidMount + componentDidUpdate")
+//   //     setTimeout(() => {
+//   //       alert("Data fetched")
+//   //     }, 3000)
+//   //   }, [count]) //? count state'i her degistiginde yukaridaki mount+update kismi calisitirilir.
+
+//   //! ComponentWillUnmount
+//   const fetchData = () => {
+//     console.log("Data Fetch started")
+//   }
+//   useEffect(() => {
+//     const timer = setInterval(fetchData, 1000)
+
+//     return () => {
+//       clearInterval(timer)
+//       console.log("componentWillUnmount")
+//     }
+//   }, [])
+
+//   const handleInc = () => {
+//     setCount(count + 1)
+//   }
+//   console.log("render")
+//   return (
+//     <div className="container text-center">
+//       <h1 className="text-danger">USEEFFECT</h1>
+//       <h3>COUNT={count}</h3>
+//       <button className="btn btn-info" onClick={handleInc}>
+//         INC
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default UseEffectHook
+
+
+// import React, { useEffect } from 'react'
+
+// const UseEffectHook = () => {
+ 
+//   useEffect(() => {
+//      // componentDidMount (Dependency Array bos ise 1 kez calisir)
+//      // bos degil ise componentDidMount + componentDidUpdate (Dependecy array bos degilse , state varsa. her state degisiminde)
+//      // Dependecy arraydeki state her degistiginde  veya ilk render sonrasis  bu kod blogu tekrardan calisisir. 
+  
+//     return () => {
+//       // cleanup function  ( componentWillUnmount)
+//     }
+//   }, [third])  // Dependency Array
+  
+
+
+//   return (
+//     <div>UseEffectHook</div>
+//   )
+// }
+
+// export default UseEffectHook
+
+
+import React, { useState } from 'react'
+
+const UseEffectHook = () => {
+
+  const [count, setCount] = useState(0)
+
+  const handleInc = () => {
+
+    setCount(count +1)
+  }
+
+
+  return (
+      <div className="container text-center">
+        <h1 className="text-danger">LIFECYCLE METHODS</h1>
+        <h3>COUNT={count}</h3>
+        <button className="btn btn-info" onClick={handleInc}>
+          INC
+        </button>
+      </div>
+    )
+}
+
+export default UseEffectHook
