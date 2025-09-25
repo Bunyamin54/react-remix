5e9034170b789ce1d31a19fba0db9d46ec8f9610
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-25 12:59:09 +0200

    Link to absolute and relative path

--- 8-Router-intro-starter/src/pages/Instructors.jsx
+++ 8-Router-intro-starter/src/pages/Instructors.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Instructors = () => {
   return (
     <div className="p-3">
       <h1>Instructors</h1>
+      <div className="d-flex flex-column">
+        {/* abdsolute path kadikoydeki rihtima git demek bu */}
+        {/* <Link to="/instructor/1">Instructor-1</Link>  */}
+
+        {/* relative path / kullnamadigimizda  */}
+        <Link to="1">Instructor-1</Link>
+        <Link to="2">Instructor-2</Link>
+        <Link to="3">Instructor-3</Link>
+      </div>
     </div>
   );
 };
