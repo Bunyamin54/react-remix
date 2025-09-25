695cb92d3d0b6ecb9219fb0ca648acddb40dd138
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-25 13:06:09 +0200

    useParams hooks

--- 8-Router-intro-starter/src/pages/InstructorDetail.jsx
+++ 8-Router-intro-starter/src/pages/InstructorDetail.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const InstructorDetail = () => {
+
+  let {id}=useParams()
+
   return (
-    <div className='p-3'>InstructorDetail</div>
+    <div className='p-3'>
+      <p>
+        from useParam : I"am instrcutur {" "}
+        <span className='text-danger fw-bolder'>{id}</span>
+      </p>
+    </div>
   )
 }
 
