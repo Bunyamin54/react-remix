b86e0982d732fa993d25e33b8e238a1d5772578b
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-25 12:48:08 +0200

    useNavigate hooks used

--- 8-Router-intro-starter/src/pages/Contact.jsx
+++ 8-Router-intro-starter/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <div className="p-3">
       <h1>Contact</h1>
       <button className="btn btn-success"  onClick={()=>navigate(-1)}>Go Back</button>
-      <button className="btn btn-primary">Go Home</button>
+      <button className="btn btn-primary" onClick={() => navigate ("/")}>Go Home</button>
     </div>
   );
 };
