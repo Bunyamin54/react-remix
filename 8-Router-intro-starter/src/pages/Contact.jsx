9c8c5112d77fef1da59ffb7f22e27d0fc1e7108c
Bünyamin Genc <bnymngnc.1660@gmail.com>
2025-09-25 12:46:04 +0200

    useNavigate hooks added

--- 8-Router-intro-starter/src/pages/Contact.jsx
+++ 8-Router-intro-starter/src/pages/Contact.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const Contact = () => {
+
+  let navigate = useNavigate()
   return (
     <div className="p-3">
       <h1>Contact</h1>
+      <button className="btn btn-success"  onClick={()=>navigate(-1)}>Go Back</button>
+      <button className="btn btn-primary">Go Home</button>
     </div>
   );
 };
